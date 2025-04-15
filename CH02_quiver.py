648f1a5d63f46acd33a741c666acba71d22126d9
s-hynes <shynes2@nd.edu>
2025-04-15 21:06:38 +0100

    Added a new function that creates phase portraits for 2D systems of linear ODEs.

--- CH02_quiver.py
+++ CH02_quiver.py
@@ -1,6 +1,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import warnings
 
 def eigen(A, print_values=False):
     """This function calculates the eigenvalues and eigenvectors of a square matrix and outputs them in an array. 
@@ -47,7 +48,8 @@ def direction_field(a,b,c,d, xmin=-5, xmax=5, ymin=-5, ymax=5, num_arrows=9):
     eigenvalues = eigen(A)[0]
 
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(6.4,6.4))
+    plt.axis('equal')
 
     ax.quiver(xx, yy, x_dot, y_dot)
     #ax.plot(x, np.zeros_like(x), 'gray')
@@ -69,12 +71,65 @@ def direction_field(a,b,c,d, xmin=-5, xmax=5, ymin=-5, ymax=5, num_arrows=9):
                 #print("Fixed point at ({0}, {1})".format(x[l], y[k]))
                 ax.plot(x[l], y[k], 'ko')    
 
+    warnings.filterwarnings("ignore", ".*artist.*")
     ax.legend()
     ax.grid()
-    #plt.axis('equal')
     ax.set_xlim(xmin, xmax)
     ax.set_ylim(ymin, ymax)
     plt.show()
 
+def stream_lin(a,b,c,d, xmin=-5, xmax=5, ymin=-5, ymax=5, num_points=101, eq_pts=[], density=1.5):
+
+    eq_pts = np.array(eq_pts)
+
+    x = np.linspace(xmin, xmax, num_points)
+    y = np.linspace(ymin, ymax, num_points)
+
+    if abs(xmax-xmin) >= (ymax-ymin):
+        man = x
+    elif abs(xmax-xmin) < (ymax-ymin):
+        man = y
+
+    xx,yy = np.meshgrid(x,y)
+
+    x_dot = a*xx + b*yy
+    y_dot = c*xx + d*yy
+
+    A = [   [a,b],
+            [c,d]]
+    
+    eigenvectors = eigen(A)[2]
+    eigenvalues = eigen(A)[0]
+
+    fig = plt.figure(figsize=(6.4,6.4))
+    plt.axis('equal')
+
+    for i in range(2):
+        if np.isreal(eigenvalues[i]):
+            if eigenvalues[i] > 0:
+                plt.plot( np.real(eigenvectors[i,0])*man, np.real(eigenvectors[i,1])*man, c='r', ls=':', lw=3, label='Unstable manifold')
+            elif eigenvalues[i] < 0:
+                plt.plot( np.real(eigenvectors[i,0])*man, np.real(eigenvectors[i,1])*man, c='g', ls=':', lw=3, label='Stable manifold')
+
+    plt.streamplot(xx, yy, x_dot, y_dot, density=density, color='grey')
+
+    for k in range(x.size):
+        for l in range(y.size):
+            #print(k, l)
+            if x_dot[k,l] == 0 and y_dot[k,l] == 0:
+                #print("Fixed point at ({0}, {1})".format(x[l], y[k]))
+                plt.plot(x[l], y[k], 'ko')    
+
+    warnings.filterwarnings("ignore", ".*artist.*")
+    try: 
+        plt.legend()
+    except:
+        pass
+    plt.grid()
+    plt.xlim(xmin, xmax)
+    plt.ylim(ymin, ymax)
+    plt.show()
+
 if __name__=="__main__":
-    direction_field( 1, 1, 4, -2, xmin=-20, xmax=20, ymin=-20, ymax=20, num_arrows=20)
+    #direction_field( 1, 1, 4, -2, xmin=-20, xmax=20, ymin=-20, ymax=20, num_arrows=20)
+    stream_lin( 1, 1, 4, -2, xmin=-20, xmax=20, ymin=-20, ymax=20, num_points=101)
