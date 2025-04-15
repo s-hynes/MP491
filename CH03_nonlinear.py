d093d43f2386ece7d129af6783651aa3cd175ea6
s-hynes <shynes2@nd.edu>
2025-04-15 21:09:07 +0100

    Added phase portrait function for nonlinear ODEs

--- CH03_nonlinear.py
+++ CH03_nonlinear.py
@@ -1,6 +1,5 @@
 """This is a version of the function that plots direction fields that works for nonlinear ODEs.
-It's a little bit clunky to use because you have to change the variable used manually within the function.
-That could definitely be improved."""
+I think that it could eventually be merged with the function for the linear case."""
 
 from CH02_quiver import eigen
 import numpy as np
@@ -40,6 +39,36 @@ def direction_field2(xdot, ydot, xmin=-5, xmax=5, ymin=-5, ymax=5, num_arrows=9)
     ax1.set_ylim(ymin, ymax)
     plt.show()
 
+
+def stream(xdot, ydot, xmin=-5, xmax=5, ymin=-5, ymax=5, num_points=100, eq_pts=[]):
+
+    eq_pts = np.array(eq_pts)
+
+    x = np.linspace(xmin, xmax, num_points)
+    y = np.linspace(ymin, ymax, num_points)
+
+    xx,yy = np.meshgrid(x,y)
+
+    x_dot = xdot(xx, yy)
+    y_dot = ydot(xx, yy)
+
+    #x_eq = np.empty_like(x_dot)
+    #y_eq = np.empty_like(y_dot)
+
+    fig = plt.figure(figsize=(10,10))
+
+
+    plt.streamplot(xx, yy, x_dot, y_dot, density=1)
+    try:
+        plt.plot(eq_pts[:,0], eq_pts[:,1], 'ko')
+    except:
+        pass
+
+    plt.axis("equal")
+    plt.grid()
+    plt.show()
+
 if __name__=="__main__":
     #direction_field2(xdot=lambda x,y: y*(13-x**2-y**2), ydot=lambda x,y: 12 - x*(13-x**2-y**2), xmin=-7, xmax=7, ymin=-6, ymax=6, num_arrows=20)
-    direction_field2(xdot=lambda x,y: x - y, ydot=lambda x,y: 1 - x*y, xmin=-10, xmax=10, ymin=-10, ymax=10, num_arrows=25)
+    #direction_field2(xdot=lambda x,y: x - y, ydot=lambda x,y: 1 - x*y, xmin=-10, xmax=10, ymin=-10, ymax=10, num_arrows=25)
+    stream(xdot=lambda x,y: x - y, ydot=lambda x,y: 1 - x*y, xmin=-4, xmax=4, ymin=-4, ymax=4, eq_pts=[ [-1,-1], [1,1]])
